930018bf8262f32757d8f99658a86fa67a272a29
Yasmin Zancan <yazancan@gmail.com>
2026-04-21 15:17:26 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="/logo.png" alt="" />
+      </header>
+      <section>
+        <img src="/banner.png" alt="" />
+      </section>
+    </main>
+  )
+}
+
+export default App
