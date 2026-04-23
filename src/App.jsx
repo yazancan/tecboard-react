ed935093657dc114afed189238aee84416a7af75
Yasmin Zancan <yazancan@gmail.com>
2026-04-23 15:32:19 -0300

    Finaliza cards e adiciona favicon

--- src/App.jsx
+++ src/App.jsx
@@ -34,13 +34,97 @@ function App() {
   ];
 
   const [eventos, setEventos] = useState([
-    {
-      capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem1.png",
-      tema: temas[0],
-      data: new Date(),
-      titulo: "Mulheres no Front",
-    },
-  ]);
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem1.png",
+    tema: temas[0],
+    data: new Date("2025-09-26"),
+    titulo: "Mulheres no Front",
+  },
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem2.png",
+    tema: temas[0],
+    data: new Date("2025-10-15"),
+    titulo: "Pixel & Code",
+  },
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem3.png",
+    tema: temas[1],
+    data: new Date("2025-10-20"),
+    titulo: "Back-End Masters",
+  },
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem4.png",
+    tema: temas[1],
+    data: new Date("2025-11-03"),
+    titulo: "Código do Coração",
+  },
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem5.png",
+    tema: temas[1],
+    data: new Date("2025-11-12"),
+    titulo: "Server Side Summit",
+  },
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem6.png",
+    tema: temas[2],
+    data: new Date("2025-11-25"),
+    titulo: "DevOps Evolution",
+  },
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem7.png",
+    tema: temas[3],
+    data: new Date("2025-12-02"),
+    titulo: "Deep Learning Days",
+  },
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem8.png",
+    tema: temas[3],
+    data: new Date("2025-12-09"),
+    titulo: "IA na palma da mão",
+  },
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem9.png",
+    tema: temas[3],
+    data: new Date("2025-12-15"),
+    titulo: "IA segura e acessível",
+  },
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem10.png",
+    tema: temas[4],
+    data: new Date("2026-01-10"),
+    titulo: "Data Pulse",
+  },
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem11.png",
+    tema: temas[4],
+    data: new Date("2026-01-18"),
+    titulo: "Data Revolution",
+  },
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem12.png",
+    tema: temas[4],
+    data: new Date("2026-01-26"),
+    titulo: "Driven by Data",
+  },
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem13.png",
+    tema: temas[4],
+    data: new Date("2026-02-03"),
+    titulo: "SQL Summit",
+  },
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem14.png",
+    tema: temas[5],
+    data: new Date("2026-02-15"),
+    titulo: "SkyTech Summit",
+  },
+  {
+    capa: "https://raw.githubusercontent.com/yazancan/tecboard-react/refs/heads/main/public/imagens-cards/imagem15.png",
+    tema: temas[5],
+    data: new Date("2026-02-22"),
+    titulo: "Mundo Cloud",
+  },
+]);
 
   function adicionarEvento(evento) {
     setEventos([...eventos, evento]);
@@ -56,7 +140,7 @@ function App() {
 
     <section className="container">
       {temas.map(function (tema) {
-        if (eventos.some(function(evento) {
+        if (!eventos.some(function(evento) {
           return evento.tema.id == tema.id
         })) {
           return null
